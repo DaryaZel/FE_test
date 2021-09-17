6d4b31d716ae6a3b5011d2a39d915918d7d6fcc3
Darya <darya@MacBook-Pro-Aliaksei-2.local>
2021-09-17 19:38:42 +0300

    fix test 5

--- test_5/main.js
+++ test_5/main.js
@@ -1,6 +1,7 @@
 async function getObject() {
     let response = await fetch('https://jsonplaceholder.typicode.com/posts')
     let data = await response.json()
+    let object
     data.map(item => {
         if (item.id == 5) {
             object = item
