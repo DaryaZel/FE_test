a2bc2d6974fa92c49f5fb2c08364f4a97c1c91f6
Darya <darya@MacBook-Pro-Aliaksei-2.local>
2021-09-17 18:27:11 +0300

    add test_4 solution

---
+++ test_4/mail.js
@@ -0,0 +1,11 @@
+function getSum(array) {
+    let sum = 0
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if (Number.isInteger(parseFloat(element))) {
+            sum += parseFloat(element)
+        }
+    }
+    console.log(sum)
+}
+getSum(['1', 'a', '25', '13.1'])
